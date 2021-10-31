f16ba7a284403745c23f642fd32e59466679e8e7
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-10-30 21:15:29 -0700

    done

--- large.c
+++ large.c
@@ -1,1 +1,26 @@
+#include <stdid.h>
+int main()
+{
+    int a;
+    a = 1000;
+    printf("value of a is %d", a);
 
+    a = -21000;
+    printf("value of a is %d", a);
+
+    printf("Value of a is %d", a);
+
+    a = 10000000;
+    printf("Value of a is %d", a);
+
+    a = -10000000;
+    printf("Value of a is %d", a);
+
+    a = 100020004000503;
+    printf("Value of a is %d", a);
+
+    a = -4325987632;
+    printf("Value of a is %d", a);
+
+    return 0;
+}
