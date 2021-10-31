d520defc2b4f29463d8fec5085e2f26abb44a4ac
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-10-30 23:48:31 -0700

    done

--- input.c
+++ input.c
@@ -3,8 +3,8 @@ int main()
 {
 
     int a,b,sum;
-    scanf("enter a number: %d", &a);
-    scanf("enter a number: %d", &b);
+    scanf(" %d", &a);
+    scanf("%d", &b);
     sum = a + b;
     printf("Sum is: %d\n",sum);
     return 0;
