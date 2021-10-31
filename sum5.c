f8bf26e97456443c591dbde4d81a8d7a6b5e32ec
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-10-30 18:22:47 -0700

    ok

--- sum5.c
+++ sum5.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main()
 {
- int a = 50, b = 60, sum;
- sum = a + b;
- printf("%d + %d = %d", a, b, sum);
- return 0;
+    int a=50, b=50, sum;
+    sum = a+b;
+    printf("%d + %d = %d",a,b,sum);
+    return 0;
 }
